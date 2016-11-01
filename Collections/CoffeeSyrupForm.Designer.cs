f88e7dc49e13ea3829bbb04ea6d6d88cff2538b2
anju chawla <anju.chawla@georgiancollege.ca>
2016-11-01 12:06:02 -0400

    Partial Starting Code

---
+++ Collections/CoffeeSyrupForm.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Collections
+{
+    partial class CoffeeSyrupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printSelectedFlavoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previewSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printAllFlavoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previewAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCoffeeFlavourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeCoffeeFlavourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearCoffeeListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countCoffeeListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(593, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printSelectedFlavoursToolStripMenuItem,
+            this.printAllFlavoursToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // printSelectedFlavoursToolStripMenuItem
+            // 
+            this.printSelectedFlavoursToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printSelectedToolStripMenuItem,
+            this.previewSelectedToolStripMenuItem});
+            this.printSelectedFlavoursToolStripMenuItem.Name = "printSelectedFlavoursToolStripMenuItem";
+            this.printSelectedFlavoursToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.printSelectedFlavoursToolStripMenuItem.Text = "Print &Selected Flavours";
+            // 
+            // printSelectedToolStripMenuItem
+            // 
+            this.printSelectedToolStripMenuItem.Name = "printSelectedToolStripMenuItem";
+            this.printSelectedToolStripMenuItem.Size = new System.Drawing.Size(196, 26);
+            this.printSelectedToolStripMenuItem.Text = "&Print Selected";
+            // 
+            // previewSelectedToolStripMenuItem
+            // 
+            this.previewSelectedToolStripMenuItem.Name = "previewSelectedToolStripMenuItem";
+            this.previewSelectedToolStripMenuItem.Size = new System.Drawing.Size(196, 26);
+            this.previewSelectedToolStripMenuItem.Text = "Preview &Selected";
+            // 
+            // printAllFlavoursToolStripMenuItem
+            // 
+            this.printAllFlavoursToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printAllToolStripMenuItem,
+            this.previewAllToolStripMenuItem});
+            this.printAllFlavoursToolStripMenuItem.Name = "printAllFlavoursToolStripMenuItem";
+            this.printAllFlavoursToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.printAllFlavoursToolStripMenuItem.Text = "Print  &All Flavours";
+            // 
+            // printAllToolStripMenuItem
+            // 
+            this.printAllToolStripMenuItem.Name = "printAllToolStripMenuItem";
+            this.printAllToolStripMenuItem.Size = new System.Drawing.Size(157, 26);
+            this.printAllToolStripMenuItem.Text = "&Print All";
+            // 
+            // previewAllToolStripMenuItem
+            // 
+            this.previewAllToolStripMenuItem.Name = "previewAllToolStripMenuItem";
+            this.previewAllToolStripMenuItem.Size = new System.Drawing.Size(157, 26);
+            this.previewAllToolStripMenuItem.Text = "Pre&view All";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(230, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCoffeeFlavourToolStripMenuItem,
+            this.removeCoffeeFlavourToolStripMenuItem,
+            this.clearCoffeeListToolStripMenuItem,
+            this.countCoffeeListToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // addCoffeeFlavourToolStripMenuItem
+            // 
+            this.addCoffeeFlavourToolStripMenuItem.Name = "addCoffeeFlavourToolStripMenuItem";
+            this.addCoffeeFlavourToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.addCoffeeFlavourToolStripMenuItem.Text = "&Add Coffee Flavour";
+            // 
+            // removeCoffeeFlavourToolStripMenuItem
+            // 
+            this.removeCoffeeFlavourToolStripMenuItem.Name = "removeCoffeeFlavourToolStripMenuItem";
+            this.removeCoffeeFlavourToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.removeCoffeeFlavourToolStripMenuItem.Text = "&Remove Coffee Flavour";
+            // 
+            // clearCoffeeListToolStripMenuItem
+            // 
+            this.clearCoffeeListToolStripMenuItem.Name = "clearCoffeeListToolStripMenuItem";
+            this.clearCoffeeListToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.clearCoffeeListToolStripMenuItem.Text = "&Clear Coffee List";
+            // 
+            // countCoffeeListToolStripMenuItem
+            // 
+            this.countCoffeeListToolStripMenuItem.Name = "countCoffeeListToolStripMenuItem";
+            this.countCoffeeListToolStripMenuItem.Size = new System.Drawing.Size(238, 26);
+            this.countCoffeeListToolStripMenuItem.Text = "Count Coffee &List";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(125, 26);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Coffee and Syrup Selection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Coffee Flavour";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "&Syrup";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(485, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Anju Chawla";
+            // 
+            // CoffeeSyrupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 312);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "CoffeeSyrupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "R \'n R for Reading and Refreshment";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printSelectedFlavoursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printSelectedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previewSelectedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printAllFlavoursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previewAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCoffeeFlavourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeCoffeeFlavourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearCoffeeListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem countCoffeeListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Collections/CoffeeSyrupForm.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Name : Anju Chawla
+ * Date : November, 2016
+ * Purpose: To allow the user to select coffee and syrup flavours.
+ * New coffee flavours can be added and old ones removed. 
+ * The entire coffee list can be cleared and number of coffee\flavours can be displayed.
+ * Can print all available coffee flavours and only selected ones too.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collections
+{
+    public partial class CoffeeSyrupForm : Form
+    {
+        public CoffeeSyrupForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
